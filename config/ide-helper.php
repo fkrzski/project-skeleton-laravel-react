e26e9b341fe0b3845ac6ad264cfab108e2cf6c21
Filip Krzyżanowski <filip.krzyzanowski@protonmail.com>
2025-12-08 06:54:54 +0100

    Pint code styles; Parallel pint tests; Composer script for lint all files

--- config/ide-helper.php
+++ config/ide-helper.php
@@ -188,7 +188,7 @@ return [
     */
 
     'extra' => [
-        'Eloquent' => [\Illuminate\Database\Eloquent\Builder::class, Builder::class],
+        'Eloquent' => [Illuminate\Database\Eloquent\Builder::class, Builder::class],
         'Session' => [Store::class],
     ],
 
